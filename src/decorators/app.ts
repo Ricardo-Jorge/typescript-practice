ca336ef4786c75e1ac9a481421bb85355d0febfd
Ricardo Jorge <ricardolemos18@hotmail.com>
2025-10-25 11:28:06 -0300

    Decorators with Typescript

---
+++ src/decorators/app.ts
@@ -0,0 +1,23 @@
+@decorator
+export class Animal {
+  constructor(public nome: string, public cor: string) {}
+}
+
+function decorator<T extends new (...args: any[]) => any>(target: T) {
+  return class extends target {
+    nome: string;
+    cor: string;
+    constructor(...args: any[]) {
+      super(...args);
+      this.nome = this.inverte(args[0]);
+      this.cor = this.inverte(args[1]);
+    }
+
+    inverte(valor: string) {
+      return valor.split("").reverse().join("");
+    }
+  };
+}
+const animal = new Animal("Ricardo", "azul");
+
+console.log(animal);
